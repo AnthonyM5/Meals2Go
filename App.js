7cb394f482141633c63ecd4e28f37338a479ebd3
AnthonyM5 <anthony.mai530@gmail.com>
2021-07-17 18:06:17 -0400

    moved navigation stack to MainNavigation.js

--- App.js
+++ App.js
@@ -1,41 +1,17 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import Home from "../Meals2Go/src/screens/Home";
-import Search from "../Meals2Go/src/screens/Search";
-import { Button } from "react-native";
+// import { createStackNavigator } from "@react-navigation/stack";
+// import Home from "../Meals2Go/src/screens/Home";
+// import Search from "../Meals2Go/src/screens/Search";
+// import { Button } from "react-native";
+import MainNavigator from "./src/navigation/MainNavigation";
 
-const Stack = createStackNavigator();
+// const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={({ navigation }) => ({
-            headerRight: () => (
-              <Button
-                title="Search"
-                onPress={() => navigation.navigate("Search")}
-              />
-            ),
-          })}
-        />
-        <Stack.Screen
-          name="Search"
-          component={Search}
-          options={({ navigation }) => ({
-            headerRight: () => (
-              <Button
-                title="Home"
-                onPress={() => navigation.navigate("Home")}
-              />
-            ),
-          })}
-        />
-      </Stack.Navigator>
+      <MainNavigator />
     </NavigationContainer>
   );
 };
--- src/navigation/MainNavigation.js
+++ src/navigation/MainNavigation.js
@@ -1,14 +1,42 @@
-// import { createStackNavigator } from '@react-navigation/stack';
-// import Home from '../screens/Home'
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import Home from "../screens/Home";
+import Search from "../screens/Search";
+import { Button } from "react-native";
 
-// const Stack = createStackNavigator()
+const Stack = createStackNavigator();
 
-// const MainNavigator = () => {
-//     return (
-//         <Stack.Navigator>
-//             <Stack.Screen name="Home" component={Home} />
-//         </Stack.Navigator>
-//     )
-// }
+const MainNavigator = () => {
+  return (
+    <>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={({ navigation }) => ({
+            headerRight: () => (
+              <Button
+                title="Search"
+                onPress={() => navigation.navigate("Search")}
+              />
+            ),
+          })}
+        />
+        <Stack.Screen
+          name="Search"
+          component={Search}
+          options={({ navigation }) => ({
+            headerRight: () => (
+              <Button
+                title="Home"
+                onPress={() => navigation.navigate("Home")}
+              />
+            ),
+          })}
+        />
+      </Stack.Navigator>
+    </>
+  );
+};
 
-// export default MainNavigator;
+export default MainNavigator;
